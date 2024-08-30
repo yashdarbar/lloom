5c52b1d0df3823eace337269cd027a6d97178f81
Yash Darbar <darbaryash44@gmail.com>
2024-08-31 00:55:09 +0530

    prisma error resolved

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+"use client";
+
+import { PrismaClient } from "@/prisma/src/app/generated/client";
+
+const prismaClientSingleton = () => {
+    return new PrismaClient();
+};
 
 declare global {
-    var cachedPrisma: PrismaClient;
+    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-} else {
-    if (!global.cachedPrisma) {
-        global.cachedPrisma = new PrismaClient();
-    }
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-    prisma = global.cachedPrisma;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
 export const db = prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//     var cachedPrisma: PrismaClient;
+// }
+
+// let prisma: PrismaClient;
+// if (process.env.NODE_ENV === "production") {
+//     prisma = new PrismaClient();
+// } else {
+//     if (!global.cachedPrisma) {
+//         global.cachedPrisma = new PrismaClient();
+//     }
+
+//     prisma = global.cachedPrisma;
+// }
+
+// export const db = prisma;
