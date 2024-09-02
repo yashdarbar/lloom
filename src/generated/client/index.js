246c4894dd591c17549f7100d6ae6e4126e97779
Yash Darbar <darbaryash44@gmail.com>
2024-09-02 21:21:35 +0530

    commit

--- src/generated/client/index.js
+++ src/generated/client/index.js
@@ -215,7 +215,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
